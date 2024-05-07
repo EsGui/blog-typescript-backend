d1581f46acbd667bbf48e2e73e34d6107037f8f8
EsGui <guilhermedagama2021100@gmail.com>
2024-05-07 00:23:31 -0300

    entendendo mongoose

--- src/connection/ConnectionDB.ts
+++ src/connection/ConnectionDB.ts
@@ -1,17 +1,26 @@
 import { MongoClient } from "mongodb";
+import mongoose, { Schema } from "mongoose";
+import UserSchema from "./UserSchema";
 
 class ConnectionDB {
 
-    public client = new MongoClient("mongodb://localhost:27017/");
+    // public client = new MongoClient("mongodb://localhost:27017/");
 
-    public connectToDataBase() {
-        try {
-            const database = this.client.db('blog');
-            const users = database.collection('users');
-            return users;
-        } catch (error) {
-            console.error(`ERROR ====>>>> ${error}`);
-        }
+    // public connectToDataBase() {
+    //     try {
+    //         const database = this.client.db('blog');
+    //         const users = database.collection('users');
+    //         return users;
+    //     } catch (error) {
+    //         console.error(`ERROR ====>>>> ${error}`);
+    //     }
+    // }
+
+    public db = mongoose.connect("mongodb://localhost:27017/blog");
+    
+    public pushBank() {
+        const users = mongoose.model('users', UserSchema);
+        return users;
     }
 }
 
--- src/controllers/LoginController.ts
+++ src/controllers/LoginController.ts
@@ -1,11 +1,12 @@
 import ConnectionDB from "../connection/ConnectionDB";
+import { Request, Response } from "express";
 
 class LoginController {
 
     public async listUsers(req: Request, res: Response) {
-        const users = new ConnectionDB().connectToDataBase()
+        const users = new ConnectionDB().pushBank()
 
-        const request = await users.find({name: "Guilherme"}).toArray();
+        const request = await users?.find({ name: "Guilherme" });
 
         return res.status(200).json(request);
     }
